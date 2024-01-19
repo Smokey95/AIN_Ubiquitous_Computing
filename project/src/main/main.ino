c588033987d63c87e172f1ffc279f66267eeb509
Smokey95 <97231524+Smokey95@users.noreply.github.com>
2024-01-19 20:43:06 +0100

    project - add serial initialization

--- project/src/main/main.ino
+++ project/src/main/main.ino
@@ -14,8 +14,8 @@
  * Constants
  ******************************************************************************/
 const int PIN_RED_LED     = 2;        /**< GPIO pin for red LED */
-const int PIN_BLUE_LED    = 3;        /**< GPIO pin for blue LED */  
-const int PIN_GREEN_LED   = 4;        /**< GPIO pin for green LED */
+const int PIN_GREEN_LED   = 3;        /**< GPIO pin for green LED */
+const int PIN_BLUE_LED    = 4;        /**< GPIO pin for blue LED */
 
 /*******************************************************************************
  * Setup
@@ -25,7 +25,7 @@ void setup() {
   init_LED();
 
   // Initialize serial communication
-  Serial.begin(9600);
+  init_serial();
 }
 
 /*******************************************************************************
@@ -40,6 +40,9 @@ void loop() {
  * Init Functions
  ******************************************************************************/
 
+/**
+ * @brief   Initializes the internal LED
+*/
 void init_LED() {
   // Initialize LEDs
   pinMode(PIN_RED_LED, OUTPUT);
@@ -56,9 +59,37 @@ void init_LED() {
   setColor(0, 0, 0);
 }
 
+/**
+ * @brief   Initializes the serial communication
+*/
+void init_serial() {
+  Serial.begin(9600);
+  // wait for 2 seconds for serial port to connect
+  delay(2000);
+  Serial.println("Serial initialized");
+  blink_x_times(3, 0, 0, 255);
+}
+
 /*******************************************************************************
- * Helper Functions
+ * LED Functions
  ******************************************************************************/
+
+/**
+ * @brief   Blinks the internal LED x times
+ * @param   x   Number of times to blink
+ * @param   R   Red value (0-255)
+ * @param   G   Green value (0-255)
+ * @param   B   Blue value (0-255)
+ * @note    Blocking function for x * 500ms 
+*/
+void blink_x_times(int x, int R, int G, int B) {
+  for (int i = 0; i < x; i++){
+    setColor(R, G, B);
+    delay(250);
+    setColor(0, 0, 0);
+    delay(250);
+  }
+}
 
 /**
  * @brief   Sets the color of the RGB LED
