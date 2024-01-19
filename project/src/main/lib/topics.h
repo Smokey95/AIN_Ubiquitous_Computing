64e8a428a11ac8936b9fdff63c1f76e627a7e926
Smokey95 <97231524+Smokey95@users.noreply.github.com>
2024-01-19 23:04:00 +0100

    project - add door sensor

--- project/src/main/lib/topics.h
+++ project/src/main/lib/topics.h
@@ -1,3 +1,5 @@
 
-#define TP_ALIVE    "letterbox_alive"
-#define TP_LED      "letterbox_led"
+#define TP_ALIVE          "letterbox_alive"
+#define TP_LED            "letterbox_led"
+#define TP_DOOR_MAIN      "letterbox_door_main"
+#define TP_DOOR_LETTER    "letterbox_door_letter"
