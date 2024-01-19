3a9925334939841476326c2967a6d8f20b97f6c0
Smokey95 <97231524+Smokey95@users.noreply.github.com>
2024-01-19 20:19:52 +0100

    project - add led init

--- project/src/main/main.ino
+++ project/src/main/main.ino
@@ -8,21 +8,22 @@
  * Includes
  ******************************************************************************/
 #include <WiFiNINA.h>
-
+#include "lib\credentials.h"
 
 /*******************************************************************************
  * Constants
  ******************************************************************************/
-const int lock_gpio_out  = 2;                          /**< GPIO pin for lock */
+const int PIN_RED_LED     = 2;        /**< GPIO pin for red LED */
+const int PIN_BLUE_LED    = 3;        /**< GPIO pin for blue LED */  
+const int PIN_GREEN_LED   = 4;        /**< GPIO pin for green LED */
 
 /*******************************************************************************
  * Setup
  ******************************************************************************/
 void setup() {
-  // Initialize internal LED to indicate when unlocked
-  pinMode(LEDR, OUTPUT);
-  // Initialize GPIO for lock
-  pinMode(lock_gpio_out, OUTPUT);
+  // Initialize internal LED
+  init_LED();
+
   // Initialize serial communication
   Serial.begin(9600);
 }
@@ -33,4 +34,40 @@ void setup() {
 
 void loop() {
 
+}
+
+/*******************************************************************************
+ * Init Functions
+ ******************************************************************************/
+
+void init_LED() {
+  // Initialize LEDs
+  pinMode(PIN_RED_LED, OUTPUT);
+  pinMode(PIN_BLUE_LED, OUTPUT);
+  pinMode(PIN_GREEN_LED, OUTPUT);
+
+  // Blink once to indicate start
+  setColor(255, 0, 0);
+  delay(250);
+  setColor(0, 255, 0);
+  delay(250);
+  setColor(0, 0, 255);
+  delay(250);
+  setColor(0, 0, 0);
+}
+
+/*******************************************************************************
+ * Helper Functions
+ ******************************************************************************/
+
+/**
+ * @brief   Sets the color of the RGB LED
+ * @param   R   Red value (0-255)
+ * @param   G   Green value (0-255)
+ * @param   B   Blue value (0-255)
+*/
+void setColor(int R, int G, int B) {
+  analogWrite(PIN_RED_LED, 255 - R);
+  analogWrite(PIN_GREEN_LED, 255 - G);
+  analogWrite(PIN_BLUE_LED, 255 - B);
 }
